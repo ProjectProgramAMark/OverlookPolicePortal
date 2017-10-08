e2568f0ca6e548410ff5df4e5de6b74b818e9bc2
Mark Moussa <markmoussa@Marks-MacBook-Pro.local>
2017-10-08 13:36:37 -0400

    WORK WORK WORK WORK WORK WORK

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,73 @@
 <template>
-  <div><span>Hello World!</span></div>
+  <div class="col-md-6 offset-md-3">
+
+    <table class="table table-inverse col-md-6 offset-md-3">
+    <thead>
+      <tr>
+        <th>Location Address</th>
+        <th>Location Name</th>
+        <th>Lat/Long</th>
+        <th>Remove</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(data, index) in arr" v-if="arr.removed">
+        <td>{{data.location_address}}</td>
+        <td>{{data.location_name}}</td>
+        <td>{{data.lat}} "," {{data.long}}</td>
+        <td><button v-on:click="removeSkimmer(data.uuid, index)">Remove</button></td>
+      </tr>
+    </tbody>
+</table>
+
+
+
+</div>
 </template>
 
 <script>
+import Vue from 'vue'
 import Axios from 'axios'
 
 export default {
   name: 'HelloWorld',
+  data(){
+    return{
+      responseData: [],
+      arr: []
+    }
+  },
   methods: {
-    someMethod() {
-      console.log('ayyy lmao')
-      Axios.get('https://overlook-api.herokuapp.com/get-all-skimmers', {
-        headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
-      }).then(function(res) {
-        console.log('Response', res);
+    getAll: function() {
+      let that = this;
+      Axios.get('http://overlook-api.herokuapp.com/get-all-skimmers').then(function(res) {
+        that.responseData = res.data;
+        console.log('Response', JSON.stringify(res.data));
+        Object.keys(that.responseData).forEach(function (key) {
+            var val = that.responseData[key];
+            that.arr.push(val);
+            // use val
+        });
       }).catch(function(err) {
+        console.log("There is an error here");
         console.log(err);
       })
+    },
+    removeSkimmer: function(UUID, index) {
+      let that = this;
+      Axios.post('http://overlook-api.herokuapp.com/skimmer-removed', {uuid: UUID}).then(function(res) {
+        console.log("This is the response for remove function: ", res);
+        if(res.data.removed) {
+          that.arr.splice(index, 1);
+        }
+      }).catch(function(err) {
+        console.log("This is an error");
+        console.log(err);
+      });
     }
   },
   created: function() {
-    this.someMethod();
+    this.getAll();
   }
 }
 </script>
